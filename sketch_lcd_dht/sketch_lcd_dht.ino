ba13b5f42a40de1da3c421c6c7c30771103d2e44
gawadinc <gawad@ukr.net>
2016-12-04 21:45:35 +0200

    Add files via upload

---
+++ sketch_lcd_dht/sketch_lcd_dht.ino
@@ -0,0 +1,78 @@
+/*
+  Measurement temperature and humidity
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Temp: 24.10 Hum: 50.05" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+#include <DHT.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+#define DHTPIN 7 // номер пина, к которому подсоединен датчик
+// Раскомментируйте в соответствии с используемым датчиком
+// Инициируем датчик
+
+DHT dht(DHTPIN, DHT22);
+//DHT dht(DHTPIN, DHT11);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  
+}
+
+void loop() {
+  
+  //Считываем влажность
+  float h = dht.readHumidity();
+
+  // Считываем температуру
+  float t = dht.readTemperature();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(t);
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  lcd.print("Hum: ");
+  // print the number of seconds since reset:
+  lcd.print(h);
+}
+
